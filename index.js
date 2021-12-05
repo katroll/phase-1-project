b64dfebbe59c855bb8d753526224f9aacf27875c
TheOrangeDot <theorangedottwitch@gmail.com>
2021-12-06 05:43:49 +0900

    Clean up styling for reusability, added a 'can't find word' action

--- index.js
+++ index.js
@@ -1,73 +1,84 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    initSearchBar();
+  initSearchBar();
 }
 
 function initSearchBar() {
-    const searchForm = document.querySelector('#search-form');
+  const searchForm = document.querySelector("#search-form");
 
-    searchForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        getDefinition(searchForm.querySelector('#word-search').value);
-        e.target.reset();
-    });
+  searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    getDefinition(searchForm.querySelector("#word-search").value);
+    e.target.reset();
+  });
 }
 
 function getDefinition(word) {
-    const wordDefList = document.querySelector('#word-def-list');
+  const wordDefList = document.querySelector("#word-def-list");
 
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-    .then(resp => resp.json()) 
-    .then(wordInfo => {
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then((resp) => resp.json())
+    .then((wordInfo) => {
+      if (
+        wordInfo.message ===
+        "Sorry pal, we couldn't find definitions for the word you were looking for."
+      ) {
+        document.querySelector("#word-def-list").innerHTML = "";
+        document.querySelector("#word-value").textContent = wordInfo.message;
+      } else {
         newSearch(wordInfo);
         addSearchHist(wordInfo);
+      }
     });
 }
- 
 
 function newSearch(wordInfo) {
-    const wordDefList = document.querySelector('#word-def-list');
-    wordDefList.innerHTML = '';
-    
-    wordInfo.forEach(word => loadWord(word));
+  const wordDefList = document.querySelector("#word-def-list");
+  wordDefList.innerHTML = "";
 
+  wordInfo.forEach((word) => loadWord(word));
 }
 
 function loadWord(wordInfo) {
-    const meaningsArray = wordInfo.meanings;
-    const wordValue = document.querySelector('#word-value');
-    const wordDefList = document.querySelector('#word-def-list');
+  const meaningsArray = wordInfo.meanings;
+  const wordValue = document.querySelector("#word-value");
+  const wordDefList = document.querySelector("#word-def-list");
 
-    wordValue.textContent = wordInfo.word;
+  wordValue.textContent = wordInfo.word;
 
-    meaningsArray.forEach(meaning => {
-        const partOfSpeech = document.createElement('h4');
-        partOfSpeech.textContent = meaning.partOfSpeech
-        wordDefList.appendChild(partOfSpeech);
-        loadDef(meaning);
-    });
-    
+  meaningsArray.forEach((meaning) => {
+    const partOfSpeech = document.createElement("h4");
+    partOfSpeech.textContent = meaning.partOfSpeech;
+    wordDefList.appendChild(partOfSpeech);
+    loadDef(meaning);
+  });
 }
 
 function loadDef(meaning) {
-    const wordDefList = document.querySelector('#word-def-list');
+  const wordDefList = document.querySelector("#word-def-list");
 
-    meaning.definitions.forEach(def => {
-        const newDef = document.createElement('li');
-        newDef.textContent = def.definition;
-        wordDefList.appendChild(newDef);
-
-    })
+  meaning.definitions.forEach((def) => {
+    const newDef = document.createElement("li");
+    newDef.textContent = def.definition;
+    wordDefList.appendChild(newDef);
+  });
 }
 
 function addSearchHist(wordInfo) {
-    const searchList = document.querySelector('#search-hist-list');
-    const newWord = document.createElement('li');
+  const searchList = document.querySelector("#search-hist-list");
+  const newWord = document.createElement("li");
 
-    newWord.textContent = wordInfo[0].word;
-    newWord.addEventListener('click', (e) => newSearch(wordInfo));
-    searchList.appendChild(newWord);
-
+  newWord.textContent = wordInfo[0].word;
+  newWord.classList.add("history-li");
+  newWord.addEventListener("click", (e) => newSearch(wordInfo));
+  searchList.appendChild(newWord);
 }
 
+// const getPicture = () => {
+//   fetch(
+//     `https://www.brandonfowler.me/gimgapi/?q=${word}&num=10&size=&color=&reuse=&type=&time=&format=read/1/`
+//   )
+//     .then((res) => res.json())
+//     .then((img) => console.log(img));
+// };
